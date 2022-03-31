b635a8e38c04e34692d5a0c150642af924f3707d
sara johansson <sara.johanss0n@hotmail.com>
2022-03-31 21:27:35 +0200

    Add project files.

---
+++ MinAdressbok/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿
+namespace MinAdressbok
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtZipCode = new System.Windows.Forms.TextBox();
+            this.txtStreeAdress = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.ContactListbox = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(22, 363);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(221, 31);
+            this.txtCity.TabIndex = 23;
+            // 
+            // txtZipCode
+            // 
+            this.txtZipCode.Location = new System.Drawing.Point(22, 302);
+            this.txtZipCode.Name = "txtZipCode";
+            this.txtZipCode.Size = new System.Drawing.Size(221, 31);
+            this.txtZipCode.TabIndex = 22;
+            // 
+            // txtStreeAdress
+            // 
+            this.txtStreeAdress.Location = new System.Drawing.Point(22, 240);
+            this.txtStreeAdress.Name = "txtStreeAdress";
+            this.txtStreeAdress.Size = new System.Drawing.Size(221, 31);
+            this.txtStreeAdress.TabIndex = 21;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(22, 178);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(221, 31);
+            this.txtEmail.TabIndex = 20;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(22, 53);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(221, 31);
+            this.txtName.TabIndex = 19;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(22, 115);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(221, 31);
+            this.txtPhone.TabIndex = 18;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(773, 14);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(150, 31);
+            this.txtSearch.TabIndex = 17;
+            // 
+            // ContactListbox
+            // 
+            this.ContactListbox.FormattingEnabled = true;
+            this.ContactListbox.ItemHeight = 25;
+            this.ContactListbox.Location = new System.Drawing.Point(274, 75);
+            this.ContactListbox.Name = "ContactListbox";
+            this.ContactListbox.Size = new System.Drawing.Size(832, 379);
+            this.ContactListbox.TabIndex = 16;
+            this.ContactListbox.SelectedIndexChanged += new System.EventHandler(this.ContactListbox_SelectedIndexChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(22, 412);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(221, 46);
+            this.btnAdd.TabIndex = 15;
+            this.btnAdd.Text = "Spara kontakt";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(929, 460);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(153, 46);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "Ta bort";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(929, 6);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(153, 46);
+            this.btnSearch.TabIndex = 12;
+            this.btnSearch.Text = "Sök";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 25);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Namn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 25);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Telefon";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 25);
+            this.label3.TabIndex = 26;
+            this.label3.Text = "Epost";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 25);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Gatuadress";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(268, 298);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 25);
+            this.label5.TabIndex = 28;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 274);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(113, 25);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "Postnummer";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(25, 335);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(69, 25);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "Postort";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(586, 6);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(153, 46);
+            this.btnClear.TabIndex = 31;
+            this.btnClear.Text = "Töm fälten";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1118, 513);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtZipCode);
+            this.Controls.Add(this.txtStreeAdress);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.ContactListbox);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "Form1";
+            this.Text = "Adressbok";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtZipCode;
+        private System.Windows.Forms.TextBox txtStreeAdress;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.ListBox ContactListbox;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ MinAdressbok/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MinAdressbok
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ContactListbox.Items.Clear();
+            LoadContacts();
+            ContactListbox.Items.AddRange(contactList.ToArray());
+
+        }
+
+        class Person
+        {
+            public string Namn { get; set; }
+            public string Telefon { get; set; }
+            public string Epost { get; set; }
+            public string Gatuadress { get; set; }
+            public string Postnummer { get; set; }
+            public string Postort { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Namn}\t{Telefon}\t{Epost}\t{Gatuadress}\t{Postnummer}\t{Postort}";
+            }
+        }
+
+        
+        List<Person> contactList = new List<Person>();
+        string path = "adresser.txt";
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {   
+            if (txtName.Text == "" || txtPhone.Text == "" || txtEmail.Text == "" || txtStreeAdress.Text == "" || txtZipCode.Text == "" || txtCity.Text == "")
+            {
+                MessageBox.Show("Alla fällt måste vara ifyllda");
+            }
+            else
+            {
+                Person contact = (Person)ContactListbox.SelectedItem;
+
+                if (contact == null)
+                {
+                    contact = new Person
+                    {
+                        Namn = txtName.Text,
+                        Telefon = txtPhone.Text,
+                        Epost = txtEmail.Text,
+                        Gatuadress = txtStreeAdress.Text,
+                        Postnummer = txtZipCode.Text,
+                        Postort = txtCity.Text
+                        
+                        
+                    };
+                    contactList.Add(contact);
+                }
+                else
+                {
+                    contact.Namn = txtName.Text;
+                    contact.Telefon = txtPhone.Text;
+                    contact.Epost = txtEmail.Text;
+                    contact.Gatuadress = txtStreeAdress.Text;
+                    contact.Postnummer = txtZipCode.Text;
+                    contact.Postort = txtCity.Text;
+                }
+                
+                ContactListbox.Items.Clear(); 
+                ContactListbox.Items.AddRange(contactList.ToArray());
+
+                SaveContacts();
+
+                
+                
+                txtStreeAdress.Text = "";
+                txtPhone.Text = "";
+                txtEmail.Text = "";
+                txtName.Text = "";
+                txtZipCode.Text = "";
+                txtCity.Text = "";
+
+                txtSearch.Text = "";
+            }
+
+          
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e) 
+        {
+            
+            List<Person> matchedContacts = new List<Person>();                  
+            foreach (Person contact in contactList)
+            {
+                if (contact.Postort.ToLower().Contains(txtSearch.Text.ToLower()))
+                {
+                    matchedContacts.Add(contact);
+                }
+            }
+
+            foreach (Person contact in contactList)
+            {
+                if (contact.Namn.ToLower().Contains(txtSearch.Text.ToLower()))
+                {
+                    matchedContacts.Add(contact);
+                }
+            }
+
+            if (matchedContacts.Count > 0)
+            {
+                ContactListbox.Items.Clear();
+                ContactListbox.Items.AddRange(matchedContacts.ToArray());
+            }
+            else
+            {
+                MessageBox.Show("Finns inte!");
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e) 
+        {
+            
+            if (ContactListbox.SelectedItems.Count > 0)
+            {
+                
+                foreach (Person contact in ContactListbox.SelectedItems)
+                {
+                    
+                    contactList.Remove(contact);
+                }
+
+                SaveContacts();   
+                ContactListbox.Items.Clear();
+                ContactListbox.Items.AddRange(contactList.ToArray());
+                txtSearch.Clear();
+
+                txtStreeAdress.Text = ""; 
+                txtPhone.Text = "";
+                txtEmail.Text = "";
+                txtName.Text = "";
+                txtZipCode.Text = "";
+                txtCity.Text = "";
+            }
+            else
+            {
+                
+                MessageBox.Show("Välj kontakt att radera");
+            }
+        }
+        
+       
+        private void SaveContacts() 
+        {
+            try //för att programmet ej ska krascha om det inte finns en fil att hämta data ifrån
+            {
+                using StreamWriter fileWriter = new StreamWriter(path);
+
+                foreach (Person contact in contactList)
+                {
+                    fileWriter.WriteLine(contact.Namn);
+                    fileWriter.WriteLine(contact.Telefon);
+                    fileWriter.WriteLine(contact.Epost);
+                    fileWriter.WriteLine(contact.Gatuadress);
+                    fileWriter.WriteLine(contact.Postnummer);
+                    fileWriter.WriteLine(contact.Postort);
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+               
+           
+
+        }
+
+        private void LoadContacts() 
+        {
+            contactList.Clear();
+
+            try 
+            {
+                using StreamReader fileReader = new(path);
+
+                string firstName = fileReader.ReadLine();
+
+                while (firstName != null)
+                {
+                    
+                    contactList.Add(new Person()
+                    {
+                        Namn = firstName,
+                        Telefon = fileReader.ReadLine(),
+                        Epost = fileReader.ReadLine(),
+                        Gatuadress = fileReader.ReadLine(),
+                        Postnummer = fileReader.ReadLine(),
+                        Postort = fileReader.ReadLine()
+                    });
+
+                    firstName = fileReader.ReadLine();
+                }
+            }
+            catch(IOException)
+            {
+
+            }
+            
+            
+                     
+
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtSearch.Clear();
+
+            txtStreeAdress.Text = "";
+            txtPhone.Text = "";
+            txtEmail.Text = "";
+            txtName.Text = "";
+            txtZipCode.Text = "";
+            txtCity.Text = "";
+
+            
+            ContactListbox.Items.Clear();
+            ContactListbox.Items.AddRange(contactList.ToArray());
+        }
+
+        private void ContactListbox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            
+            if (ContactListbox.SelectedIndex == -1)
+                return;
+            var contact = (Person)ContactListbox.SelectedItem;
+             
+                      
+            txtName.Text = contact.Namn;
+            txtPhone.Text = contact.Telefon;
+            txtEmail.Text = contact.Epost;
+            txtStreeAdress.Text = contact.Gatuadress;
+            txtZipCode.Text = contact.Postnummer;
+            txtCity.Text = contact.Postort;
+
+        }
+    }
+}
